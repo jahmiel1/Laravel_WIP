941b521ef89259f5103c098d35184022fb730e20
Kevando Gordon <kevandogordon@yahoo.com>
2021-09-25 10:24:07 -0500

    Admin page update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,34 +16,38 @@
     <div class=" bg-white p-4 h-full w-2/12">
         <h1 class="text-3xl font-extrabold text-center text-blue-600 mb-6">Admin</h1>
         <ul>
+            <a href="{{route('Admin')}}">
             <li @if (!request()->routeIs('Admin'))
                 class="py-2 px-2 hover:bg-blue-600 duration-300 mt-3  hover:text-white cursor-pointer"
             @else
                 class="py-2 px-2 bg-blue-600 text-white cursor-pointer"
             @endif>
-                <a href="{{route('Admin')}}">Students</a> </li>
+                Students</li></a> 
+            <a href="{{route('course')}}">
             <li @if (!request()->routeIs('course'))
                 class="py-2 px-2 hover:bg-blue-600 duration-300  hover:text-white cursor-pointer"
             @else
                 class="py-2 px-2 bg-blue-600 text-white cursor-pointer"
             @endif>
-                <a href="{{route('course')}}">Courses</a></li>
+                Courses</li></a>
+            <a href="{{route('courseType')}}">
             <li @if (!request()->routeIs('course_type'))
                 class="py-2 px-2 hover:bg-blue-600 duration-300  hover:text-white cursor-pointer"
             @else
                 class="py-2 px-2 bg-blue-600 text-white cursor-pointer"
             @endif>
-                <a href="{{route('courseType')}}">Course Type</a></li>
+                Course Type</li></a>
+            <a href="{{route('courseSelection')}}">
             <li @if (!request()->routeIs('course_selection'))
                 class="py-2 px-2 hover:bg-blue-600 duration-300  hover:text-white cursor-pointer"
             @else
                 class="py-2 px-2 bg-blue-600 text-white cursor-pointer"
             @endif>
-                <a href="{{route('courseSelection')}}">Course Selection</a></li>
+                Course Selection</li></a>
             <form>@csrf
             <li class="py-2 px-2 hover:bg-blue-600 duration-300 hover:text-white cursor-pointer"> 
                 <button
-                    class="pr-10 focus:outline-none"
+                    class="pr-10 w-full text-left focus:outline-none"
                     formaction="{{route('Logout')}}"
                     formmethod="post"
                     type="submit"
